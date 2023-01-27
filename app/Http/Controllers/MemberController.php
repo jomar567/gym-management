bac53a90914e6f3d33c75dab0e61499875312e6e
jomar567 <cladojomar881@gmail.com>
2023-01-27 12:54:37 +0800

    view all trainers

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 use App\Models\Member;
+use App\Models\Trainer;
 
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
     public function index() {
-        return view('index')->with('members', Member::orderByDesc('created_at')->get());
+        $members = Member::latest()->get();
+        $trainers = Trainer::all();
+        return view('index')->with('members', $members)->with('trainers', $trainers);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,14 +57,14 @@
                                             <td>{{ $member->trainer->name }}</td>
                                             <td>{{ $member->trainer->specialization }}</td>
                                             <td>{{ $member->membership->membership_type }}</td>
-                                            {{-- <td class="d-flex flex-row gap-2 justify-content-center align-items-center">
-                                                <a href={{ route('functions.showStudent', $student->id) }} class="btn btn-secondary">View</a>
-                                                <a href={{ route('functions.editStudent', $student->id) }} class="btn btn-primary">Edit</a>
-                                                <form action={{ route('destroy', $student->id) }} method="POST" class="d-inline-block">
+                                            <td class="d-flex flex-row gap-2 justify-content-center align-items-center">
+                                                {{-- <a href={{ route('functions.showMember', $member->id) }} class="btn btn-secondary">View</a> --}}
+                                                {{-- <a href={{ route('functions.editMember', $member->id) }} class="btn btn-primary">Edit</a>
+                                                <form action={{ route('destroy', $member->id) }} method="POST" class="d-inline-block">
                                                     @csrf
                                                     <button class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </td> --}}
+                                                </form> --}}
+                                            </td>
                                         </tr>
                                     @endforeach
                                 @else
@@ -79,7 +79,47 @@
                     </div>
                   </div>
                   <div class="tab-pane fade" id="trainers" role="tabpanel">
-                     <h5>Trainers</h5>
+                    <div class="overflow-auto">
+                        <table class="table table-light table-striped shadow">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Specialization</th>
+                                    <th>Phone</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                @if(count($trainers) > 0)
+                                    @foreach($trainers as $trainer)
+                                        <tr>
+                                            <th scope="row">{{ $trainer->id }}</th>
+                                            <td>{{ $trainer->name }}</td>
+                                            <td>{{ $trainer->email }}</td>
+                                            <td>{{ $trainer->specialization }}</td>
+                                            <td>{{ $trainer->phone }}</td>
+                                            <td class="d-flex flex-row gap-2 justify-content-center align-items-center">
+                                                {{-- <a href={{ route('functions.showMember', $member->id) }} class="btn btn-secondary">View</a> --}}
+                                                {{-- <a href={{ route('functions.editMember', $member->id) }} class="btn btn-primary">Edit</a>
+                                                <form action={{ route('destroy', $member->id) }} method="POST" class="d-inline-block">
+                                                    @csrf
+                                                    <button class="btn btn-danger">Delete</button>
+                                                </form> --}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="6" class="text-center">
+                                            No Member Available
+                                        </td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                   </div>
                   <div class="tab-pane fade" id="memberships" role="tabpanel">
                      <h5>Membership</h5>
